d226dbdf0baf1c1751d52d91a23e0e2b2526abcc
Crypto Gnome <33667144+CryptoGnome@users.noreply.github.com>
2023-02-15 09:31:10 -0500

    Update sleep time

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ async function main(){
     }
     
     //after all players are updated, sleep for 5 seconds then run main again
-    await sleep(15000);
+    await sleep(60000);
     main();
 
 
